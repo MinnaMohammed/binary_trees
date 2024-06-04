fb17c88d4611e6167cff511f96eb80e92407d8ea
MinnaMohammed <barakatminna1@gmail.com>
2024-06-04 15:53:49 -0700

    [9. Height]

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left;
 	size_t right;
 
+	if (!tree)
+		return (0);
+
 	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
